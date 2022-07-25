7ad020793a251b625f07752cfcae22acfc378125
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-25 16:48:46 -0500

    add misc and chrome macros

---
+++ macros/mac-chrome.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2022 by Jessica E. Calderon
+#
+# SPDX-License-Identifier: MIT
+
+# MACROPAD Hotkeys example: Chrome web browser for Mac
+
+from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
+
+app = {                    # REQUIRED dict, must be named 'app'
+    'name': 'Mac Chrome',  # Application name
+    'macros': [           # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x004000, '< Back', [Keycode.COMMAND, '[']),
+        (0x004000, 'Fwd >', [Keycode.COMMAND, ']']),
+        (0x400000, 'Up', [Keycode.SHIFT, ' ']),      # Scroll up
+        # 2nd row ----------
+        (0x202000, '< Tab', [Keycode.CONTROL, Keycode.SHIFT, Keycode.TAB]),
+        (0x202000, 'Tab >', [Keycode.CONTROL, Keycode.TAB]),
+        (0x400000, 'Down', ' '),                     # Scroll down
+        # 3rd row ----------
+        (0x000040, 'Reload', [Keycode.COMMAND, 'r']),
+        (0x000040, 'Home', [Keycode.COMMAND, 'H']),
+        (0x000040, 'Private', [Keycode.COMMAND, Keycode.SHIFT, 'N']),
+        # 4th row ---------- # can configure to custom websites
+        (0x000000, 'UTSA', [Keycode.COMMAND, 'n', -Keycode.COMMAND,
+                            'https://courses.bootcampspot.com/\n']),   # UTSA Bootcamp in new window
+        (0x800000, 'Github', [Keycode.COMMAND, 'n', -Keycode.COMMAND,
+                              'www.github.com\n']),   # Github in new window
+        (0x101010, 'local', [Keycode.COMMAND, 'n', -Keycode.COMMAND,
+                             'http://localhost:3001/\n']),  # localhost in new win
+        # Encoder button ---
+        (0x000000, '', [Keycode.COMMAND, 'w'])  # Close window/tab
+    ]
+}
---
+++ macros/mac-misc.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2022 Jessica Calderon
+#
+# SPDX-License-Identifier: MIT
+
+# MACROPAD Hotkeys: Mac Misc Shortcuts for Mac
+
+
+from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from consumer import Toolbar
+
+app = {                      # REQUIRED dict, must be named 'app'
+    'name' : 'misc', # Application name
+    'order': 0,
+    'macros' : [             # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x095E06, 'Open  ', [Keycode.COMMAND, 'o']),
+        (0x544408, 'Save', [Keycode.COMMAND, 's']),
+        (0x540908, 'Quit  ', [Keycode.COMMAND, 'q']),
+        # 2nd row ----------
+        (0x544408, 'All', [Keycode.COMMAND, 'a']),
+        (0x544408, 'Copy', [Keycode.COMMAND, 'c']),
+        (0x544408, 'Cut  ', [Keycode.COMMAND, 'x']),
+        # 3rd row ----------
+        (0x000000, '  ', []),
+        (0x544408, 'Paste', [Keycode.COMMAND, 'p']),
+        (0x000000, '  ', []),
+        # 4th row ----------
+        (0x080F54, '', []),
+        (0x000000, '       ', []),
+        (0x540908, 'Force Quit', [Keycode.COMMAND, Keycode.OPTION, Keycode.ESCAPE]),
+        # Encoder button ---
+        (0x000000, '', [Keycode.COMMAND, 'w']) # Close window/tab
+    ]
+}
