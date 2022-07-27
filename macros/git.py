324a1984765601abf813356141f229d73f1f8870
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-27 01:44:24 -0500

    git macro

--- macros/git.py
+++ macros/git.py
@@ -8,25 +8,26 @@ from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
 
 app = {                      # REQUIRED dict, must be named 'app'
     'name' : 'git', # Application name
+        'order': 0,
     'macros' : [             # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x004000, 'Add', ['git add .']),
-        (0x004000, 'Commit', ['git commit -m ']),
-        (0x004000, 'Push', ['git push ']),
+        (0x004000, 'add', ['git add .\n']),
+        (0x004000, 'commit', ['git commit -m ']),
+        (0x004000, 'push', ['git push origin ']),
         # 2nd row ----------
-        (0xff0076, 'origin', ['origin ']),
-        (0x004000, 'main', ['main']),
-        (0xff0076, 'Pull', ['git pull ']),
+        (0xff0076, 'pull', ['git pull origin ']),        
+        (0xff0076, 'main', ['main\n']),
+        (0x004000, 'develop', ['develop\n']),
         # 3rd row ----------
-        (0x0a0dd8, 'Merge', ['git merge ']),
-        (0x0a0dd8, 'Checkout', ['git checkout ']),
+        (0x0a0dd8, 'merge', ['git merge ']),
+        (0x0a0dd8, 'checkout', ['git checkout ']),
         (0x0a0dd8, '-b', ['-b ']),
         # 4th row ----------
-        (0xc70000, 'Status', ['git status']),
-        (0xc70000, 'Branch', ['git branch']),
-        (0xFFFF00,  'Clone', ['git clone ']),
+        (0xc70000, 'status', ['git status']),
+        (0xc70000, 'branch', ['git branch']),
+        (0xFFFF00,  'clone', ['git clone ']),
         # Encoder button ---
-        (0x000000, '', [Keycode.COMMAND, 'w']) # Close window/tab
+        (0x000000, '', [Keycode.ENTER]) # ENTER key
     ]
 }
